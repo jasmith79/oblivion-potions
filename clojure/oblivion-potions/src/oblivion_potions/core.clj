1e2ad71324503d4dae1252b4ed688fb0ace69b49
jsmith <jarsmith@indot.in.gov>
2016-03-30 14:56:29 -0400

    Still working on it

--- clojure/oblivion-potions/src/oblivion_potions/core.clj
+++ clojure/oblivion-potions/src/oblivion_potions/core.clj
@@ -46,12 +46,12 @@
 (println "Reading file...")
 
 (def ingredients
-  (filter
+  (vec (filter
     (fn [line]
       ;; clojure is smart enough here to discard the empty lines, so no need to check the length
       ;; like javascript, however needs the unicode handling for some reason.
       (not (re-find #"(?u),[ \t\xA0\u1680\u180e\u2000-\u200a\u202f\u205f\u3000]*," line)))
-    (str/split (slurp "../../allingredients.csv") #"\n")))
+    (str/split (slurp "../../allingredients.csv") #"\n"))))
 
 (println "done in " (quot (- (now) start) 1000) " seconds.")
 
@@ -60,6 +60,7 @@
 ;;; Returns first match for negative effects, else nil
 (defn negative? [effect] (re-find #"(?i)damage|drain|paralyze|burden|silence" effect))
 
+;;; Scores a potion or combo based on the effects
 (defn score
   [effs]
   (reduce
@@ -69,7 +70,7 @@
     0
     effs))
 
-;;; copied from rosetta code
+;;; Copied from rosetta code
 (defn combinations
   "If m=1, generate a nested list of numbers [0,n)
    If m>1, for each x in [0,n), and for each list in the recursion on [x+1,n), cons the two"
@@ -87,7 +88,7 @@
 ; (defn make-potion
 ;   "creates a potion data structure"
 ;   [name & ingreds]
-;   (identity name))
+;   (first ingreds))
 (defrecord Potion [name effects recipe positives negatives score])
 (defn make-potion
   "creates a potion data structure"
@@ -97,27 +98,27 @@
         (fn [accum ingred]
           (let [[ingred_name & ingred_effects] (.split ingred ",")
                 [acc_names acc_effects] accum]
-            (vector (conj acc_names ingred_name) (conj acc_effects ingred_effects))))
+            (vector (str acc_names ingred_name) (conj acc_effects ingred_effects))))
         ["" #{}]
         ingreds)
       [negatives positives] ((juxt filter remove) negative? effects)]
 
-      (->Potion name effects recipe))); positives negatives (score effects))))
+      (->Potion name effects recipe positives negatives (score effects))))
 
 (println "Defining potions...")
-; (def potions
-;   (filter
-;     (fn [p]
-;       (> (:score p) MIN_POTION_SCORE))
-;     (map
-;       (fn [combo]
-;         (apply make-potion (apply str combo) (map (fn [n] (get ingredients n)) combo)))
-;       (combinations 4 (count ingredients)))))
 (def potions
-  (map
-    (fn [combo]
-      (apply make-potion (apply str combo) (map (fn [n] (get ingredients n)) combo)))
-    (combinations 4 (count ingredients))))
+  (filter
+    (fn [p]
+      (> (:score p) MIN_POTION_SCORE))
+    (map
+      (fn [combo]
+        (apply make-potion (apply str combo) (map (fn [n] (get ingredients n)) combo)))
+      (combinations 4 (count ingredients)))))
+; (def potions
+;   (map
+;     (fn [combo]
+;       (apply make-potion (apply str combo) (map (fn [n] (get ingredients n)) combo)))
+;     (combinations 4 (count ingredients))))
 
 (println "done in " (quot (- (now) start) 1000) " seconds")
 
